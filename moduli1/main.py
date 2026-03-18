e7b0ddb1648c14929905c23c01ba54878cea47d0
unknown <Student@digitalschool.local>
2026-03-18 20:37:44 +0100

    lesson 1 python

---
+++ moduli1/main.py
@@ -0,0 +1,34 @@
+from PIL.DdsImagePlugin import item2
+from numpy.f2py.symbolic import as_ge
+
+temperature = 98.6
+
+name = "john"
+
+print(type(temperature))
+print(type(name))
+
+age = 30
+print(age)
+x= 5
+y= 3
+
+result = x + y
+print(result)
+
+age = age + 1
+
+print(age)
+
+first_name = "john"
+last_name = "doe"
+
+full_name = first_name + "" + last_name
+
+print(full_name)
+
+name_of_the_list = ["item 1","item2","item3","item 4"]
+
+shoping_list = ["apples", "bananas",3, 4.5]
+
+print(shoping_list[1])

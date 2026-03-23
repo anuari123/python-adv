8414efeab89b4259eec9201ce210e808019d937c
anuari123 <anuarkiqmari1@gmail.com>
2026-03-23 20:45:10 +0100

    moduli2

---
+++ moduli 2/tuples.py
@@ -0,0 +1,7 @@
+words = ("spam","eggs","sausages")
+print(words[0])
+
+person = ("Alice",30,"engineer")
+
+name,age,profesion = person
+print(name,"'s", "profession is","and she is" ,age,"years old")
--- moduli1/main.py
+++ moduli1/main.py
@@ -1,4 +1,5 @@
 from PIL.DdsImagePlugin import item2
+from keras.src.callbacks.tensorboard import keras_model_summary
 from numpy.f2py.symbolic import as_ge
 
 temperature = 98.6
@@ -32,3 +33,19 @@ name_of_the_list = ["item 1","item2","item3","item 4"]
 shoping_list = ["apples", "bananas",3, 4.5]
 
 print(shoping_list[1])
+
+shoping_list.append("oranges")
+print(shoping_list)
+
+shoping_list.insert(2,"lemons")
+print(shoping_list)
+
+shoping_list.remove("oranges")
+print(shoping_list)
+
+del shoping_list[2]
+print(shoping_list)
+
+shoping_list[0] = "cherry"
+print(shoping_list)
+

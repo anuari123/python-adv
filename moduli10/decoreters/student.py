82bb7a3fc48fd88a17464cea632192ca7164318f
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-04-29 20:29:02 +0200

    moduli10

---
+++ moduli10/decoreters/student.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self,name,age):
+        self.__name = name
+        self.__age = age
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self,name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+student1 = Student("Alice",20)
+
+print(student1.name())
+student1.name("Andi")
+print(student1.name())
+
+print(student1.age())
